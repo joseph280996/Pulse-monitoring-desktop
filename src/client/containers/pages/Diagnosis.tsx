e9d3a846c3d47b2f1acf0eb5ee4896c86459b3a2
Tung Pham <joseph.pham280996@gmail.com>
2020-12-26 01:24:43 -0500

    Add ToolBar for Diagnosis Page

--- src/client/containers/pages/Diagnosis.tsx
+++ src/client/containers/pages/Diagnosis.tsx
@@ -1,12 +1,22 @@
 import React, { ReactElement } from 'react'
+import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from 'react-bootstrap'
 
 function Diagnosis(): ReactElement {
   return (
     <>
       <div>
-        <h1>Pulse Monitoring</h1>
+        <div>Chart</div>
       </div>
-      <div>Diagnosis Page</div>
+      <div className="Diagnosis-toolbarContainer">
+        <Button type="button" className="Diagnosis-toolbarButton">
+          <FontAwesomeIcon icon={faStop} />
+        </Button>
+        <Button type="button" className="Diagnosis-toolbarButton">
+          <FontAwesomeIcon icon={faPlay} />
+        </Button>
+      </div>
     </>
   )
 }
