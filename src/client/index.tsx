22c957fb827cdafdafe573c4c883328c43cb4575
Tung Pham <joseph.pham280996@gmail.com>
2021-07-30 23:29:51 -0400

    Fix name mismatch

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import App from './App'
-import './app.global.scss'
+import './App.global.scss'
 
 render(<App />, document.getElementById('root'))
