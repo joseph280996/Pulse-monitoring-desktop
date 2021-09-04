7984662a3a2a1dbb22cc25d35ab23d94fc145db2
Tung Pham <joseph.pham280996@gmail.com>
2021-09-04 01:33:09 -0400

    Update width of days in week datepicker

--- src/renderer/client/components/form/ExportDataForm/ExportDataForm.tsx
+++ src/renderer/client/components/form/ExportDataForm/ExportDataForm.tsx
@@ -95,7 +95,6 @@ const ExportDataFormComponent = ({
               </div>
             )
           }}
-          monthsShown={2}
           selectsRange
           selected={startDate}
           onChange={onDateChange}
