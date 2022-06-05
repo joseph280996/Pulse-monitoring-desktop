29c52222ff0c73eeb68ba47abba34fc58a3adfd2
Tung Pham <joseph.pham280996@gmail.com>
2022-06-05 14:55:25 -0400

    Move sign in to auth folder + common/styles to styles

--- src/renderer/client/pages/auth/AuthRoutes.tsx
+++ src/renderer/client/pages/auth/AuthRoutes.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import useAuthState from 'renderer/client/utils/hooks/useAuthState';
-import SignInPage from '../../components/pages/signin';
+import SignInPage from './signin';
 import ChangeUsername from './ChangeUsername';
 import ForgotPass from './ForgotPass';
 
